7738cac16c2e50d252a71c408c695f69ef46312a
Junser Hwang <hzser123@gmail.com>
2023-08-24 02:31:52 +0900

    [CHORE] BUILD target jvm이 잘못 설정 되어있던 문제 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,11 +68,3 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
